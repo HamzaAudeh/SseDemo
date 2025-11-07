308ef45768a7dbdb55392a57fe1f2d20c23968b5
Hamza Odeh <HOdeh@azm.sa>
2025-11-08 01:22:28 +0300

    Add project files.

---
+++ EventService.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SseDemo;
+
+public class EventService
+{
+    public async IAsyncEnumerable<string> GetMessages([EnumeratorCancellation] CancellationToken token)
+    {
+        var counter = 0;
+        while (!token.IsCancellationRequested)
+        {
+            var message = $"data: Message {counter++} at {DateTime.Now}\n\n";
+            yield return message;
+
+            await Task.Delay(1000, token);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+using SseDemo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<EventService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/summary", async (HttpContext context, EventService service) =>
+{
+    context.Response.Headers.Add("Content-Type", "text/event-stream");
+
+    await foreach (var message in service.GetMessages(context.RequestAborted))
+    {
+        await context.Response.WriteAsync(message);
+        await context.Response.Body.FlushAsync();
+    }
+})
+.WithName("GetSummary");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
